38b46cf30c510cc6f3591510db304928d381a28e
muazzam51214 <syedmuazzamabbas2004@gmail.com>
2023-11-29 23:17:27 +0500

    Connection to Mongo Atlas

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 import {DB_NAME} from "../constants.js";
+const PORT = process.env.PORT;
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(`\n MongoDB Connected !! DB Host : ${connectionInstance.connection.host} `)
+    console.log(`MongoDB Connected || DB Host : ${connectionInstance.connection.host}`);
   } catch (error) {
-    console.log("MongoDB Connection Error" , error);
-    process.exit(1)
+    console.log("MongoDB Connection Error", error);
+    process.exit(1);
   }
 }
 
