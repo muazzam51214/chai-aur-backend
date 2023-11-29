d709d1448aa1d988b4b34280ddd2857f0ea8f927
muazzam51214 <syedmuazzamabbas2004@gmail.com>
2023-11-30 00:07:46 +0500

    Util Code Discussion

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import connectDB from "./db/index.js"
-connectDB();
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({
+  path : './.env'
+})
+const PORT = process.env.PORT;
+connectDB()
+.then(() => {
+  app.on("error", (error) => {
+    console.log("Error", error);
+    throw error;
+  })
+  app.listen(PORT, () => {
+    console.log(`Server is running at PORT : ${PORT}`);
+  })
+})
+.catch((error) => {
+  console.log("DB Connection Error", error);
+})
